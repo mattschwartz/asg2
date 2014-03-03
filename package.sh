a8b7c50683e3a8373f8a5b2b680d1dde4c01f11e
Michael Passaloukos <michael.passaloukos@utexas.edu>
2014-03-02 19:14:18 -0600

    As turned in.

--- package.sh
+++ package.sh
@@ -9,23 +9,27 @@ cp ./plugins.cfg ./turnin-dir
 cp ./resources.cfg ./turnin-dir
 cp ./README ./turnin-dir
 cp -r ./media ./turnin-dir
-#cp ./Ball.cpp ./turnin-dir
-#cp ./Ball.h ./turnin-dir
-#cp ./PlayingField.cpp ./turnin-dir
-#cp ./PlayingField.h ./turnin-dir
+cp ./Ball.cpp ./turnin-dir
+cp ./Ball.h ./turnin-dir
+cp ./Court.cpp ./turnin-dir
+cp ./Court.h ./turnin-dir
 cp ./BaseApplication.cpp ./turnin-dir
 cp ./BaseApplication.h ./turnin-dir
 cp ./TutorialApplication.cpp ./turnin-dir
 cp ./TutorialApplication.h ./turnin-dir
 #cp ./OgreApp ./turnin-dir
-cp ./Common.h ./turnin-dir
+#cp ./Common.h ./turnin-dir
 #cp ./MinimalOgre.h ./turnin-dir
 #cp ./MinimalOgre.cpp ./turnin-dir
 cp ./PaddleController.cpp ./turnin-dir
 cp ./PaddleController.h ./turnin-dir
 cp ./Paddle.h ./turnin-dir
 cp ./Paddle.cpp ./turnin-dir
+cp ./Launcher.cpp ./turnin-dir
+cp ./Launcher.h ./turnin-dir
+cp ./SoundManager.cpp ./turnin-dir
+cp ./SoundManager.h  ./turnin-dir
+cp ./SoundEffect.cpp  ./turnin-dir
+cp ./SoundEffect.h  ./turnin-dir
 cp ./assignment2 ./turnin-dir
-cp ./Room.cpp ./turnin-dir
-cp ./Room.h ./turnin-dir
 tar -c -z -f files.tgz turnin-dir
